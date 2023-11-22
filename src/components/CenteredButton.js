4cf5e39b6c523b5dd7d3e4e3b29a4a0d7f77891a
alexandre <alexandre.benjaming@gmail.com>
2023-11-22 16:39:14 -0300

    Arquivo do centrred

--- src/components/CenteredButton.js
+++ src/components/CenteredButton.js
@@ -4,7 +4,7 @@ import { Container, Row, Button } from "react-bootstrap";
 const CenteredButton = ({ children }) => (
     <Container>
         <Row className='d-flex justify-content-center'>
-            <Button className='my-5' variant='primary' size='lg' style={{ width: '200px'}}>
+            <Button className='my-5' variant='outline-success' size='lg' style={{ width: '200px'}}>
                 {children}
             </Button>
         </Row>
